61e81f0214c6302ad15c42d0e8aa9835ee715168
guguaguguag <126437091+guguaguguag@users.noreply.github.com>
2026-06-09 22:59:20 +0800

    Update db.go

--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"fmt"
-	"strings"
 	"vmq-go/utils"
 
 	"gorm.io/driver/mysql"
@@ -41,28 +40,28 @@ func initializeData() error {
 		return nil
 	}
 	settingData := map[string]string{
-		"adminUser":     "admin",                            
-		"adminPwd":      "21232f297a57a5a743894a0e4a801fc3", 
-		"notifyUrl":     "",                                 
-		"returnUrl":     "",                                 
-		"apiSecret":     "",                                 
-		"lastHeart":     "0",                                
-		"lastPay":       "0",                                
-		"expire":        "5",                                
-		"orderType":     "1",                                
-		"orderMaxNum":   "10",                               
-		"wechatPay":     "",                                 
-		"aliPay":        "",                                 
-		"emailSMTPhost": "",                                 
-		"emailSMTPport": "",                                 
-		"emailSMTPuser": "",                                 
-		"emailSMTPpwd":  "",                                 
-		"emailSMTPfrom": "",                                 
-		"emailSMTPto":   "",                                 
-		"emailSMTPssl":  "1",                                
-		"payNotice":     "0",                                
-		"errorNotice":   "1",                                
-		"monitorNotice": "1",                                
+		"adminUser":     "admin",                            // 管理员账号
+		"adminPwd":      "21232f297a57a5a743894a0e4a801fc3", // 管理员密码
+		"notifyUrl":     "",                                 // 异步回调地址
+		"returnUrl":     "",                                 // 同步回调地址
+		"apiSecret":     "",                                 // 通讯密钥
+		"lastHeart":     "0",                                // 最后心跳时间
+		"lastPay":       "0",                                // 最后支付时间
+		"expire":        "5",                                // 过期时间 单位分钟
+		"orderType":     "1",                                // 订单区分 1：金额递减 2：金额递增
+		"orderMaxNum":   "10",                               // 同金额订单最大数量
+		"wechatPay":     "",                                 // 微信收款链接
+		"aliPay":        "",                                 // 支付宝收款链接
+		"emailSMTPhost": "",                                 // 邮箱SMTP地址
+		"emailSMTPport": "",                                 // 邮箱SMTP端口
+		"emailSMTPuser": "",                                 // 邮箱SMTP账号
+		"emailSMTPpwd":  "",                                 // 邮箱SMTP密码
+		"emailSMTPfrom": "",                                 // 邮箱SMTP发件人
+		"emailSMTPto":   "",                                 // 邮箱SMTP收件人
+		"emailSMTPssl":  "1",                                // 邮箱SMTP是否开启SSL 0否 1是
+		"payNotice":     "0",                                // 收款通知 0否 1是
+		"errorNotice":   "1",                                // 异常通知 0否 1是
+		"monitorNotice": "1",                                // 监控通知 0否 1是
 	}
 	keys, data := utils.DictionaryOrderSort(settingData)
 	for i := 0; i < len(keys); i++ {
@@ -84,9 +83,11 @@ func SetupDatabase(dsn string) error {
 	if err := InitDB(dsn); err != nil {
 		return err
 	}
+	// 迁移数据库
 	if err := Migrate(); err != nil {
 		return err
 	}
+	// 初始化数据
 	if err := initializeData(); err != nil {
 		return err
 	}
